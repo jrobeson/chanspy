89e3b9e4f8fe60486d0fe80e181532d710328379
David Scholberg <recombinant.vector@gmail.com>
2020-11-19 02:30:45 -0500

    ran prettier

--- scripts/sass.js
+++ scripts/sass.js
@@ -17,15 +17,15 @@
 //   https://github.com/davidscholberg
 
 module.exports = function (robot) {
-    robot.hear(/^a[y]{2,}$/i, function (msg) {
-        msg.send('lmao');
-    });
+  robot.hear(/^a[y]{2,}$/i, function (msg) {
+    msg.send('lmao');
+  });
 
-    robot.respond(/(hi|hello|hey|greetings|hola|aloha)/i, function (msg) {
-        msg.reply(`${msg.match[1]} (☛°ヮ°)☛`);
-    });
+  robot.respond(/(hi|hello|hey|greetings|hola|aloha)/i, function (msg) {
+    msg.reply(`${msg.match[1]} (☛°ヮ°)☛`);
+  });
 
-    robot.respond(/(fu|fuck you|you suck|sucks)/i, function (msg) {
-        msg.reply('(ಠ_ಠ)┌∩┐');
-    });
-}
+  robot.respond(/(fu|fuck you|you suck|sucks)/i, function (msg) {
+    msg.reply('(ಠ_ಠ)┌∩┐');
+  });
+};
--- scripts/weather.js
+++ scripts/weather.js
@@ -16,54 +16,58 @@
 // Author:
 //   https://github.com/davidscholberg
 
-module.exports = function(robot) {
-    const weatherbitApiKey = process.env.HUBOT_WEATHERBIT_API_KEY;
+module.exports = function (robot) {
+  const weatherbitApiKey = process.env.HUBOT_WEATHERBIT_API_KEY;
 
-    function formatWeatherData(data) {
-        let stateCode = ` ${data.state_code},`;
-        if (data.country_code !== 'US') {
-            stateCode = '';
-        }
-        return `${data.city_name},${stateCode} ${data.country_code}`
-            + ` (${data.lat},${data.lon}):`
-            + ` ${data.temp}°C (${convertCelsiusToFahrenheit(data.temp).toFixed(2)}°F),`
-            + ` ${data.rh}% humidity,`
-            + ` wind ${data.wind_cdir} at ${convertMetersPerSecondToKmPerHour(data.wind_spd).toFixed(2)}km/h`
-            + ` (${convertMetersPerSecondToMilesPerHour(data.wind_spd).toFixed(2)}mph),`
-            + ` ${data.weather.description.toLowerCase()}`;
+  function formatWeatherData(data) {
+    let stateCode = ` ${data.state_code},`;
+    if (data.country_code !== 'US') {
+      stateCode = '';
+    }
+    return (
+      `${data.city_name},${stateCode} ${data.country_code}` +
+      ` (${data.lat},${data.lon}):` +
+      ` ${data.temp}°C (${convertCelsiusToFahrenheit(data.temp).toFixed(2)}°F),` +
+      ` ${data.rh}% humidity,` +
+      ` wind ${data.wind_cdir} at ${convertMetersPerSecondToKmPerHour(data.wind_spd).toFixed(
+        2
+      )}km/h` +
+      ` (${convertMetersPerSecondToMilesPerHour(data.wind_spd).toFixed(2)}mph),` +
+      ` ${data.weather.description.toLowerCase()}`
+    );
+  }
+
+  function convertCelsiusToFahrenheit(c) {
+    return c * (9 / 5) + 32;
+  }
+
+  function convertMetersPerSecondToKmPerHour(m) {
+    return m * 3.6;
+  }
+
+  function convertMetersPerSecondToMilesPerHour(m) {
+    return m * (3600 / 1609.344);
+  }
+
+  robot.respond(/weather( for)? (.+)/i, function (msg) {
+    if (typeof weatherbitApiKey === 'undefined') {
+      console.error(`error: HUBOT_WEATHERBIT_API_KEY is not set`);
+      msg.send('error: the weather module is not configured properly');
+      return;
     }
 
-    function convertCelsiusToFahrenheit(c) {
-        return (c * (9/5)) + 32;
+    function currentWeatherResponseHandler(response) {
+      msg.send(formatWeatherData(response.data.data[0]));
     }
 
-    function convertMetersPerSecondToKmPerHour(m) {
-        return m * 3.6;
+    function weatherErrorHandler(error) {
+      msg.send('oof, there was a problem getting the weather data :(');
     }
 
-    function convertMetersPerSecondToMilesPerHour(m) {
-        return m * (3600/1609.344);
-    }
-
-    robot.respond(/weather( for)? (.+)/i, function(msg){
-        if (typeof weatherbitApiKey === 'undefined') {
-            console.error(`error: HUBOT_WEATHERBIT_API_KEY is not set`);
-            msg.send('error: the weather module is not configured properly');
-            return;
-        }
-
-        function currentWeatherResponseHandler(response) {
-            msg.send(formatWeatherData(response.data.data[0]));
-        }
-
-        function weatherErrorHandler(error) {
-            msg.send('oof, there was a problem getting the weather data :(');
-        }
-
-        // replace all whitespace with commas and then remove duplicate commas
-        const searchParam = msg.match[2].replace(/\s+/g, ',').replace(/(,)\1+/g, '$1');
-        const weatherbit = require('../lib/weatherbit');
-        weatherbit.setApiKey(weatherbitApiKey);
-        weatherbit.getCurrentWeather(searchParam, currentWeatherResponseHandler, weatherErrorHandler);
-    });
-}
+    // replace all whitespace with commas and then remove duplicate commas
+    const searchParam = msg.match[2].replace(/\s+/g, ',').replace(/(,)\1+/g, '$1');
+    const weatherbit = require('../lib/weatherbit');
+    weatherbit.setApiKey(weatherbitApiKey);
+    weatherbit.getCurrentWeather(searchParam, currentWeatherResponseHandler, weatherErrorHandler);
+  });
+};
